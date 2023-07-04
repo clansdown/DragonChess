19542f97164805cb32ea8d0585e3c7bb5e85b6cd
Ajax Lansdown <ajax@chrislansdown.com>
2023-07-04 18:01:04 -0400

    drawing cell lines (v0.1)

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -9,9 +9,22 @@ function drawBoard() {
   let canvas = document.getElementById("board");
   let ctx = canvas.getContext("2d");
   ctx.beginPath();
-  ctx.rect(20, 20, 150, 100);
+  ctx.rect(0, 0, 600, 600);
   ctx.fill();
 
+  ctx.fillStyle = "#fff";
+  ctx.beginPath();
+  for(let i = 1;i <= 8; i++){
+    ctx.rect(0, 75*i, 600, 1);
+  }
+  ctx.fill();
+  
+  ctx.fillStyle = "#fff";
+  ctx.beginPath();
+  for(let i = 1;i <= 8; i++){
+    ctx.rect(75*i, 0, 1, 600);
+  }
+  ctx.fill();
 }
 
 </script>
@@ -19,7 +32,7 @@ function drawBoard() {
 <main>
   <h1>DragonChess!</h1>
 
-  <canvas id="board" style="widht: 600px; height: 600px;"></canvas>
+  <canvas id="board" width="600" height="600" style="width: 600px; height: 600px;"></canvas>
 
 
 
@@ -27,5 +40,11 @@ function drawBoard() {
 </main>
 
 <style>
-  
+
+canvas#board {
+  border: 1px solid #f00;
+
+
+}
+
 </style>
