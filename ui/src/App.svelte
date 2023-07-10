4cc58e85a8670eb96c3fd13841641d4b8903e801
Ajax Lansdown <ajax@chrislansdown.com>
2023-07-10 14:54:07 -0400

    finally have a chessboard

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -12,19 +12,36 @@ function drawBoard() {
   ctx.rect(0, 0, 600, 600);
   ctx.fill();
 
-  ctx.fillStyle = "#fff";
+ ctx.fillStyle = "#fff";
+  ctx.beginPath();
+  for(let j = 0; j < 8; j++){
+    for(let i = 0; i <= 4; i ++){
+      let offset = 0;
+      if(j%2==1){
+        offset = 75;
+      }
+      ctx.rect(150*i+offset, 75*j, 75, 75);
+    }
+  }
+  ctx.fill();
+
+if(0) {
+  ctx.fill();
+  ctx.fillStyle = "#f00";
   ctx.beginPath();
   for(let i = 1;i <= 8; i++){
     ctx.rect(0, 75*i, 600, 1);
   }
   ctx.fill();
   
-  ctx.fillStyle = "#fff";
+  ctx.fillStyle = "#0ff";
   ctx.beginPath();
   for(let i = 1;i <= 8; i++){
     ctx.rect(75*i, 0, 1, 600);
   }
   ctx.fill();
+}
+ 
 }
 
 </script>
